915ac197496b731e96af1b0c062b8caa623bd279
Kunj Patel <pkunjk007@gmail.com>
2026-02-25 09:12:53 -0700

    Created function to pull the data

--- etl/src/load/load.py
+++ etl/src/load/load.py
@@ -18,7 +18,7 @@ from supabase import create_client, Client
 logger = logging.getLogger(__name__)
 
 # read the .env file to add values into os.environ
-env_path = Path(__file__).parent.parent.parent.parent / ".env"
+env_path = Path(__file__).parent.parent.parent / ".env"
 load_dotenv(dotenv_path=env_path)
 
 # Send only 500 rows to Supabase in one API call (avoid payload size limit)
